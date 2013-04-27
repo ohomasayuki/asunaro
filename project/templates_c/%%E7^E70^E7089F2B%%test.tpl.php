8d0bb3ec25593ef257d60425b3c21afdfd7b4f3a
web <web@xia.jp>
2013-04-28 01:51:21 +0900

    フィーチャーフォン対応を除去

---
+++ project/templates_c/%%E7^E70^E7089F2B%%test.tpl.php
@@ -0,0 +1,9 @@
+<?php /* Smarty version 2.6.18, created on 2013-04-28 01:45:48
+         compiled from ./test.tpl */ ?>
+テストです。
+<?php if ($this->_tpl_vars['v'] == 1): ?>
+v==1<br />
+<?php else: ?>
+$vは<?php echo $this->_tpl_vars['v']; ?>
+です。<br />
+<?php endif; ?>
